9d484e77cf2c252b4d34f538c6becba654584ce7
Alex Moyle <alex.moyle91@gmail.com>
2019-01-31 22:18:19 -0700

    made 100 unique, rectangle, square and random files each containing one set of numbers

--- fuzzer/fuzzer.py
+++ fuzzer/fuzzer.py
@@ -1,6 +1,4 @@
 
-
-
 import random
 
 def makeSquareCoords():
@@ -16,8 +14,8 @@ def makeSquareCoords():
             str(A + x) + " " + str(B) + " " + str(C + x) + " " + str(D + x) + " " + str(E) + " " + str(F + x) + "\n")
 
 def makeSquareOutput():
-    sqOutputFile = open("squareAutoOutput.txt", "w")
-    for x in range(50):
+    sqOutputFile = open("squareOutput.txt", "w")
+    for x in range(100):
         sqOutputFile.write("square" + "\n")
 
 def makeRectangleCoords():
@@ -33,8 +31,8 @@ def makeRectangleCoords():
             str(A + x) + " " + str(B) + " " + str(C + x) + " " + str(D + x) + " " + str(E) + " " + str(F + x) + "\n")
 
 def makeRectangleOutput():
-    rectOutputFile = open("rectangleAutoOutput.txt", "w")
-    for x in range(50):
+    rectOutputFile = open("rectangleOutput.txt", "w")
+    for x in range(100):
         rectOutputFile.write("rectangle" + "\n")
 
 def makeRandomCoords():
@@ -48,9 +46,54 @@ def make1000FilesTest():
     for x in range(1000):
        open("file%03d.txt" % x, "w").write("some text\n")
 
-makeSquareCoords()
+def makeHoweverManyFiles(amount, quadName, numString):
+    for x in range(amount):
+        open(str(quadName) + "%03d.txt" % x, "w").write(str(numString) + "\n")
+
+def makeUniqueSquareFiles():
+    A = 1
+    B = 0
+    C = 1
+    D = 1
+    E = 0
+    F = 1
+    for x in range(100):
+        open("square%03d.txt" % x, "w").write(str(A + x) + " " + str(B) + " " + str(C + x) + " " + str(D + x) + " "
+                             + str(E) + " " + str(F + x) + "\n")
+
+        # makeHoweverManyFiles(10, "square", (str(A + x) + " " + str(B) + " " + str(C + x) + " " + str(D + x) + " "
+        #                      + str(E) + " " + str(F + x) + "\n"))
+
+def makeUniqueRectangleFiles():
+    A = 1
+    B = 0
+    C = 1
+    D = 1
+    E = 0
+    F = 1
+    for x in range(100):
+        open("rectangle%03d.txt" % x, "w").write(str(A + x) + " " + str(B) + " " + str(C + x) + " " + str(D + x) + " "
+                             + str(E) + " " + str(F + x) + "\n")
+
+def makeUniqueRandomFiles():
+    A = 1
+    B = 0
+    C = 1
+    D = 1
+    E = 0
+    F = 1
+    for x in range(100):
+        open("random%03d.txt" % x, "w").write(str(random.randint(0, 100)) + " " + str(random.randint(0, 100)) + " " + str(random.randint(0, 100)) + " "
+            + str(random.randint(0, 100)) + " " + str(random.randint(0, 100)) + " " + str(random.randint(0, 100)) + "\n")
+
+
+makeUniqueSquareFiles()
+makeUniqueRectangleFiles()
+makeUniqueRandomFiles()
 makeSquareOutput()
-makeRectangleCoords()
 makeRectangleOutput()
-makeRandomCoords()
-make1000FilesTest()
+
+# makeSquareCoords()
+# makeRectangleCoords()
+# makeRandomCoords()
+# makeHoweverManyFiles("square", "2 0 2 2 0 2")
